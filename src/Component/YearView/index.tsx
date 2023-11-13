4683be43ad5569bacdca58f046424a1077a5a5ab
Kayla_Kang <Kayla_Kang@compal.com>
2023-11-13 11:10:59 +0800

    feat: add string mode and year view

--- src/Component/YearView/index.tsx
+++ src/Component/YearView/index.tsx
@@ -3,14 +3,14 @@ import { DateColorsConfig } from "../../Const/colors";
 
 interface IProps {
   isSimple?: boolean; // 是否為簡單模式
-  dateColorsConfig: DateColorsConfig; // 行事曆資料
+  dateColorsConfig: DateColorsConfig[]; // 行事曆資料
   year: number; // 年分
   onClick: (date: Date) => void; // 事件控制: 點擊事件
 }
 
 const YearView = ({ year, dateColorsConfig, isSimple, onClick }: IProps) => {
   const renderYearView = () => 
-    Array.from(Array(12), (v, k) => (
+    Array.from(Array(12), (_v, k) => (
       <MonthView
         key={+new Date(year, k)}
         monthDate={new Date(year, k)}
